b0c2ae6588375213a510e80f797bed28f73eae4c
Pierre Carles <pcarles@student.42.fr>
2017-01-14 20:18:32 +0100

    Change the dim var into preproc

--- bin/main.c
+++ bin/main.c
@@ -1,14 +1,14 @@
 #include "header.h"
+#define DIM 9
 
 int	main(int argc, char **argv)
 {
 	if(argc == 10)
 	{
-		int dim = 9;
-		int **grid = ft_init_grid(dim);
+		int **grid = ft_init_grid(DIM);
 		ft_set_grid(grid, argv);
 		ft_solve(grid, 0);
 		ft_print_grid(grid);
-		ft_free_grid(grid, dim);
+		ft_free_grid(grid, DIM);
 	}
 }
