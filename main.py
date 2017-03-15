fbc541b4bf3c41709ae2a3edc0c19a7b417c8de7
blacklennon <pcarles@student.42.fr>
2017-03-15 19:26:56 +0100

    remove some shit coz i'm a dick

--- main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3.5
-# -*- coding: utf-8 -*-
-
-from bin.solvermodule import *
-
-print(ft_solve(('.................................................................................')))
-
