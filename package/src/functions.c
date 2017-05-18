b8f25d42ab101652255949678f9eba35332a3057
bob <pcarles@student.42.fr>
2017-05-18 22:19:17 +0200

    re organization

--- package/src/functions.c
+++ package/src/functions.c
@@ -45,3 +45,29 @@ void	ft_set_grid(int **grid, char *str)
 		}
 	}
 }
+
+int	ft_check_grid(int **grid)
+{
+	int x, y, nb;
+
+	for(x = 0; x < 9; x++)
+	{
+		for(y = 0; y < 9; y++)
+		{
+			if(grid[x][y] != 0)
+			{
+				nb = grid[x][y];
+				grid[x][y] = 0;
+				if(!(ft_check_column(grid, &y, &nb) &&
+				ft_check_line(grid, &x, &nb) &&
+				ft_check_block(grid, &x, &y, &nb)))
+				{
+					grid[x][y] = nb;
+					return(0);
+				}
+				grid[x][y] = nb;
+			}
+		}
+	}
+	return(1);
+}
